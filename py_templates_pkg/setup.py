5166303c12c61eba4b824696f5abfea973483d1b
Matheus Sousa <matheussousasf@gmail.com>
2023-04-22 23:56:11 -0300

    Reconfigurando pacotes para trabalhar no novo computador

--- py_templates_pkg/setup.py
+++ py_templates_pkg/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-package_name = 'python_pkg'
+package_name = 'py_templates_pkg'
 
 setup(
     name=package_name,
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='matheus',
-    maintainer_email='matheus.soares@jacto.com.br',
+    maintainer_email='matheus@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
